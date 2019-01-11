9069daedf7bb6e240879f8de89792b2237388b11
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2019-01-11 10:03:58 -0500

    drg

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int x1,x2,y1,y2,z1,z2;
 
-        System.out.println("x for Point 1?");
+        /*System.out.println("x for Point 1?");
         x1 = scanner.nextInt();
         System.out.println("y for Point 1?");
         y1 = scanner.nextInt();
@@ -17,13 +17,20 @@ public class Main {
         System.out.println("y for Point 2?");
         y2 = scanner.nextInt();
         System.out.println("z for Point 2?");
-        z2 = scanner.nextInt();
+        z2 = scanner.nextInt();*/
+
+        x1 = 9;
+        x2 = 1;
+        y1 = 8;
+        y2 = 2;
+        z1 = 8;
+        z2 = 3;
 
         Point.pointDistance(x1,x2,y1,y2,z1,z2);
-        Point point1 = new Point();
-        Point point2 = new Point();
-        Point.verifyPoint(point1,5,9,3,point2,4,5,6);
-        System.out.println(point1.pointCreator(point1,x1,y1,z1));
-        System.out.println(point2.pointCreator(point2,5,7,9));
+        Point point1 = new Point(x1,y1,z1);
+        Point point2 = new Point(x2,y2,z2);
+        Point.verifyPoint(point1);
+        Point.verifyPoint(point2);
+
     }
 }
